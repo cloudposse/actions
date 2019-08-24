d1ffe15f91f4117f432668fe515ab94d3f9d29cd
Erik Osterman <erik@cloudposse.com>
2019-08-24 00:20:02 -0700

    Fix env

--- git-push/entrypoint.sh
+++ git-push/entrypoint.sh
@@ -4,7 +4,7 @@ set -x
 
 git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
 git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-git remote set-url origin "https://${GITHUB_ACTOR}:${{secrets.GITHUB_TOKEN}}@github.com/${GITHUB_REPOSITORY}.git"
+git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 git remote -v
 git status
 git diff --exit-code || ( git add . && git commit -m "Updated README" && git push origin HEAD:${GITHUB_HEAD_REF} )
