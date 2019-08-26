7576c81af72c45cfccd222ce30babe88f2a4fa74
Erik Osterman <erik@cloudposse.com>
2019-08-26 16:30:21 -0700

    Add git-push action (#1)
    * Add git-push action
    
    * Update LICENSE

---
+++ git-push/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+export GIT_COMMIT_MESSAGE="${GIT_COMMIT_MESSAGE:-autocommit}"
+
+git diff --exit-code 
+
+if [ $? -ne 0 ]; then
+  echo "Changes detected."
+  git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
+  git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
+
+  git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git remote -v
+  git add .
+  git commit -m "${GIT_COMMIT_MESSAGE}"
+  git push origin HEAD:${GITHUB_HEAD_REF}
+else
+  echo "No changes detected."
+fi
