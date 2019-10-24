94f29c677e210d7270a77d43d6e3e4b8e3f97618
aknysh <andriy.knysh@gmail.com>
2019-10-23 21:44:29 -0400

    Add actions

--- go/build-go-binaries/entrypoint.sh
+++ go/build-go-binaries/entrypoint.sh
@@ -8,20 +8,24 @@ export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 export GOX_OSARCH="${GOX_OSARCH:-windows/386 windows/amd64 freebsd/arm netbsd/386 netbsd/amd64 netbsd/arm linux/s390x linux/arm darwin/386 darwin/amd64 linux/386 linux/amd64 freebsd/amd64 freebsd/386 openbsd/386 openbsd/amd64}"
 
-if [[ -z "$WORKING_DIR" ]]; then
-  echo "Missing WORKING_DIR env variable"
-  exit 1
-fi
-
 if [[ -z "$OUTPUT_PATH" ]]; then
   echo "Missing OUTPUT_PATH env variable"
   exit 1
 fi
 
+pwd
+ls
+
 go get -u github.com/mitchellh/gox
 go get -u github.com/golang/dep/cmd/dep
-mkdir -p "${WORKING_DIR}" || exit 1
-cp -r . "${WORKING_DIR}" || exit 1
-cd "${WORKING_DIR}" || exit 1
+
+if [[ ! -z "$WORKING_DIR" ]]; then
+  mkdir -p "${WORKING_DIR}" || exit 1
+  cp -r . "${WORKING_DIR}" || exit 1
+  cd "${WORKING_DIR}" || exit 1
+  pwd
+  ls
+fi
+
 dep ensure
 gox -osarch="${GOX_OSARCH}" -output "${OUTPUT_PATH}{{.OS}}_{{.Arch}}"
