82a93f71e94a930fb65f6fdb9ee4d1c7f11b3539
aknysh <andriy.knysh@gmail.com>
2019-10-23 21:55:37 -0400

    Add actions

--- go/build-go-binaries/entrypoint.sh
+++ go/build-go-binaries/entrypoint.sh
@@ -6,7 +6,7 @@ export GO111MODULE=off
 export CGO_ENABLED=0
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
-export GOX_OSARCH="${GOX_OSARCH:-windows/386 windows/amd64 freebsd/arm netbsd/386 netbsd/amd64 netbsd/arm linux/s390x linux/arm darwin/386 darwin/amd64 linux/386 linux/amd64 freebsd/amd64 freebsd/386 openbsd/386 openbsd/amd64}"
+export GOX_OSARCH="${GOX_OSARCH:-linux/386 linux/amd64}"
 
 if [[ -z "$OUTPUT_PATH" ]]; then
   echo "Missing OUTPUT_PATH env variable"
