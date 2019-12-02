c53f3f78f0f8a92cd37546b84ad8b957fe8ca28d
aknysh <andriy.knysh@gmail.com>
2019-12-01 22:49:57 -0500

    [go/build] Make `GO111MODULE` configurable

--- go/build/entrypoint.sh
+++ go/build/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-export GO111MODULE=on
+export GO111MODULE="${GO111MODULE:-on}"
 export CGO_ENABLED=0
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
@@ -16,14 +16,19 @@ fi
 pwd
 ls
 
-go get -u github.com/mitchellh/gox
-
 if [[ ! -z "$WORKING_DIR" ]]; then
   mkdir -p "${WORKING_DIR}" || exit 1
   cp -r . "${WORKING_DIR}" || exit 1
   cd "${WORKING_DIR}" || exit 1
   pwd
   ls
+fi
+
+go get -u github.com/mitchellh/gox
+
+if [[ "$GO111MODULE" != "on" ]]; then
+  go get -u github.com/golang/dep/cmd/dep
+  dep ensure
 fi
 
 gox -osarch="${GOX_OSARCH}" -output "${OUTPUT_PATH}{{.OS}}_{{.Arch}}"
