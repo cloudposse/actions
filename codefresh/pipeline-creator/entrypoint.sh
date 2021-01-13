d5a98307d37134dcfc6d13cfa2744ed3822fa206
aknysh <andriy.knysh@gmail.com>
2021-01-13 11:01:52 -0500

    Add `pipeline-creator` GitHub action

---
+++ codefresh/pipeline-creator/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+git clone "${REPO}"
+git checkout "${VERSION}"
+
+codefresh auth create-context context --api-key "$CF_API_KEY"
+codefresh auth use-contex context
+
+for spec in $(tr , ' ' <<<"${SPECS}"); do
+  codefresh create pipeline -f "${CATALOG}"/"${spec}".yml
+done
