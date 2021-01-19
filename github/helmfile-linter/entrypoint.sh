4290e7e8052a27916bb2bf711ac4a6a08dfb8cd5
Dylan Bannon <dylanbannon5@gmail.com>
2021-01-19 10:10:57 -0800

    downloading helmfile in the Dockerfile instead of entrypoint.sh

--- github/helmfile-linter/entrypoint.sh
+++ github/helmfile-linter/entrypoint.sh
@@ -2,14 +2,10 @@
 
 set -x
 
-# download helmfile
-curl https://github.com/roboll/helmfile/releases/download/v0.137.0/helmfile_linux_amd64
-chmod 764 helmfile_linux_amd64
-
 # Gather list of helmfiles in current project according to the error text of `helmfile lint`:
-# "It must be named helmfile.d/*.{yaml,yml}, helmfile.yaml, or charts.yaml, or otherwise
-# specified with the --file flag"
-# However, we won't find helmfiles with custom names (specified using the --file flag in the
+#   "It must be named helmfile.d/*.{yaml,yml}, helmfile.yaml, or charts.yaml, or otherwise
+#   specified with the --file flag"
+# However, we won't find the helmfiles with custom names (specified using the --file flag in the
 # helmfile command).
 helmfiles=( $(find . -type f -name "helmfile.yaml") )
 for new_helmfile in $(find . -type f -name "charts.yaml"); do
@@ -23,7 +19,7 @@ done
 echo "Linting helmfiles."
 for unlinted_helmfile in helmfiles; do
     echo "$unlinted_helmfile"
-    ./helmfile_linux_amd64 $unlinted_helmfile
+    /helmfile_linux_amd64 $unlinted_helmfile
     echo "\n"
 done
 echo "\n"
