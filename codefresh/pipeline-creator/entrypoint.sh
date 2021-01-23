75f8ef88d18d3f5d3ce412b77d355b87a08b1e0c
Andriy Knysh <aknysh@users.noreply.github.com>
2021-01-23 11:04:02 -0500

    Update codefresh/pipeline-creator/entrypoint.sh
    Co-authored-by: Erik Osterman (CEO @ Cloud Posse) <erik@cloudposse.com>

--- codefresh/pipeline-creator/entrypoint.sh
+++ codefresh/pipeline-creator/entrypoint.sh
@@ -13,7 +13,9 @@ codefresh auth create-context context --api-key "$INPUT_CF_API_KEY"
 codefresh auth use-contex context
 
 for spec in $(tr , ' ' <<<"${INPUT_SPECS}"); do
-  gomplate -d pipeline=./"${INPUT_PIPELINE_CATALOG}"/"${spec}".yaml -f ./"${INPUT_SPEC_CATALOG}"/"${spec}".yaml -o ./"${INPUT_SPEC_CATALOG}"/"${spec}"-rendered.yaml
+  input_file=./"${INPUT_PIPELINE_CATALOG}"/"${spec}".yaml
+  output_file=./"${INPUT_SPEC_CATALOG}"/"${spec}"-rendered.yaml
+  gomplate -d pipeline=$input_file -f $input_file -o $output_file
   codefresh get project "${INPUT_PROJECT}" || codefresh create project "${INPUT_PROJECT}"
-  codefresh replace pipeline -f ./"${INPUT_SPEC_CATALOG}"/"${spec}"-rendered.yaml || codefresh create pipeline -f ./"${INPUT_SPEC_CATALOG}"/"${spec}"-rendered.yaml
+  codefresh replace pipeline -f $output_file || codefresh create pipeline -f $output_file
 done
