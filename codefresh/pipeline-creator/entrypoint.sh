ae9271e77ac6130f3f6b7680700b200d06b81200
aknysh <andriy.knysh@gmail.com>
2021-01-24 09:26:10 -0500

    Add `pipeline-creator` GitHub action

--- codefresh/pipeline-creator/entrypoint.sh
+++ codefresh/pipeline-creator/entrypoint.sh
@@ -4,17 +4,17 @@ set -e -o pipefail
 
 export PROJECT="${INPUT_PROJECT}"
 
-git clone "${INPUT_REPO_URL}"
-cd "${INPUT_REPO_NAME}"
-git checkout "${INPUT_VERSION}"
+git clone "${INPUT_CF_REPO_URL}"
+cd "${INPUT_CF_REPO_NAME}"
+git checkout "${INPUT_CF_VERSION}"
 
 codefresh auth create-context context --api-key "$INPUT_CF_API_KEY"
 codefresh auth use-contex context
 
-for spec in $(tr , ' ' <<<"${INPUT_SPECS}"); do
-  input_file=./"${INPUT_PIPELINE_CATALOG}"/"${spec}".yaml
-  output_file=./"${INPUT_SPEC_CATALOG}"/"${spec}"-rendered.yaml
+for spec in $(tr , ' ' <<<"${INPUT_CF_SPECS}"); do
+  input_file=./"${INPUT_CF_PIPELINE_CATALOG}"/"${spec}".yaml
+  output_file=./"${INPUT_CF_SPEC_CATALOG}"/"${spec}"-rendered.yaml
   gomplate -d pipeline=$input_file -f $input_file -o $output_file
-  codefresh get project "${INPUT_PROJECT}" || codefresh create project "${INPUT_PROJECT}"
+  codefresh get project "${INPUT_CF_PROJECT}" || codefresh create project "${INPUT_CF_PROJECT}"
   codefresh replace pipeline -f $output_file || codefresh create pipeline -f $output_file
 done
