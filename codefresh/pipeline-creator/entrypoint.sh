9648e1d1785f6f7cc2cafd919eb142bbc9d1bc76
Andriy Knysh <aknysh@users.noreply.github.com>
2021-01-30 01:43:05 -0500

    Update Codefresh `pipeline-creator` (#97)
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`
    
    * Update `pipeline-creator`

--- codefresh/pipeline-creator/entrypoint.sh
+++ codefresh/pipeline-creator/entrypoint.sh
@@ -5,17 +5,20 @@ set -e -o pipefail
 export PROJECT="${INPUT_CF_PROJECT}"
 export REPO="${INPUT_REPO}"
 
-git clone "${INPUT_CF_REPO_URL}"
-cd "${INPUT_CF_REPO_NAME}"
-git checkout "${INPUT_CF_VERSION}"
+NETRC="$HOME/.netrc"
+printf "machine github.com\n" > "$NETRC"
+printf "login %s\n" "$GITHUB_USER" >> "$NETRC"
+printf "password %s\n" "$GITHUB_TOKEN" >> "$NETRC"
 
-codefresh auth create-context context --api-key "$INPUT_CF_API_KEY"
-codefresh auth use-contex context
+echo "Cloning repo ${INPUT_CF_REPO_URL} version ${INPUT_CF_REPO_VERSION}"
+git clone -c advice.detachedHead=false --depth=1 -b "${INPUT_CF_REPO_VERSION}" "${INPUT_CF_REPO_URL}" codefresh
+cd ./codefresh
 
 for spec in $(tr , ' ' <<<"${INPUT_CF_SPECS}"); do
-  input_pipeline_file=./"${INPUT_CF_PIPELINE_CATALOG}"/"${spec}".yaml
-  input_spec_file=./"${INPUT_CF_SPEC_CATALOG}"/"${spec}".yaml
-  output_file=./"${INPUT_CF_SPEC_CATALOG}"/"${spec}"-rendered.yaml
+  echo "Updating '${spec}' pipeline..."
+  input_pipeline_file=./pipelines/"${INPUT_CF_SPEC_TYPE}"/"${spec}".yaml
+  input_spec_file=./specs/"${INPUT_CF_SPEC_TYPE}"/"${spec}".yaml
+  output_file=./specs/"${INPUT_CF_SPEC_TYPE}"/"${spec}"-rendered.yaml
   gomplate -d pipeline="$input_pipeline_file" -f "$input_spec_file" -o "$output_file"
   codefresh get project "${INPUT_CF_PROJECT}" || codefresh create project "${INPUT_CF_PROJECT}"
   codefresh replace pipeline -f "$output_file" || codefresh create pipeline -f "$output_file"
