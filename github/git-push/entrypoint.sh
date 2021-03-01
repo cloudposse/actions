660bdb3a378b7aef9aff0e89ca42107ce19064cd
Dylan Bannon <dylanbannon5@gmail.com>
2021-03-01 09:04:50 -0600

    adding -C option to git-push action

--- github/git-push/entrypoint.sh
+++ github/git-push/entrypoint.sh
@@ -2,19 +2,20 @@
 
 set -x
 export GIT_COMMIT_MESSAGE="${GIT_COMMIT_MESSAGE:-autocommit}"
+export GIT_DIRECTORY="${GIT_DIRECTORY:-$(pwd)}"
 
-git diff --exit-code 
+git -C ${GIT_DIRECTORY} diff --exit-code
 
 if [ $? -ne 0 ]; then
   echo "Changes detected."
-  git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
-  git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
+  git -C ${GIT_DIRECTORY} config user.name "$(git -C ${GIT_DIRECTORY} --no-pager log --format=format:'%an' -n 1)"
+  git -C ${GIT_DIRECTORY} config user.email "$(git -C ${GIT_DIRECTORY} --no-pager log --format=format:'%ae' -n 1)"
 
-  git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  git remote -v
-  git add .
-  git commit -m "${GIT_COMMIT_MESSAGE}"
-  git push origin HEAD:${GITHUB_HEAD_REF}
+  git -C ${GIT_DIRECTORY} remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  git -C ${GIT_DIRECTORY} remote -v
+  git -C ${GIT_DIRECTORY} add .
+  git -C ${GIT_DIRECTORY} commit -m "${GIT_COMMIT_MESSAGE}"
+  git -C ${GIT_DIRECTORY} push origin HEAD:${GITHUB_HEAD_REF}
 else
   echo "No changes detected."
 fi
