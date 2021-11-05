7dfe7371922365371889561b1a2186c979ad7260
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-05 18:56:01 -0500

    rewriting label requirements identification step

--- github/status-check-labeling/identify_required_pr_labels.sh
+++ github/status-check-labeling/identify_required_pr_labels.sh
@@ -1,17 +1,17 @@
 # I'm expecting some greps to fail, so we want to continue running whenever we encounter errors.
 set +e
 
-# Debug
-echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
-
 # We're starting with a pull request.
 PR_NUMBER=${GITHUB_EVENT_NUMBER}
 # Get the most recent commit on this pull request.
-MOST_RECENT_SHA=$(curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/commits | jq .[-1].sha)
+MOST_RECENT_SHA=$(curl \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/commits | \
+    jq .[-1].sha)
 MOST_RECENT_SHA_DIGEST=$(echo $MOST_RECENT_SHA | cut -c -7) 
 
-# start out assuming no plan changes are present
-PLAN_CHANGES=0
+# start out assuming the label is not needed, since we don't yet know whether the checks of interest are even present
+LABEL_NEEDED=0
 # get a list of all the check suites associated with this commit
 CHECK_SUITES=$(curl -H "Authorization: token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${MOST_RECENT_SHA_DIGEST}/check-suites | jq .check_suites[].id)
 CHECK_SUITES_ARRAY=( $CHECK_SUITES )
@@ -29,18 +29,21 @@ for check_suite in "${CHECK_SUITES_ARRAY[@]}"; do
     for run_index in $(seq 0 $LAST_RUN_INDEX); do
       echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].name
       echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].name | grep -q "${INPUTS_CHECK_NAME}"
-      # if "${{ inputs.check-name }}" is found in the run name, check this run's title for "${{ inputs.check-description }}"
+      # if "${{ inputs.check-name }}" is found in the run name,
+      # first set LABEL_NEEDED=1 (since we only potentially need a label when the relevant checks actuall took place),
+      # and then check this run's title for "${{ inputs.check-description }}" (when we might set LABEL_NEEDED back to 0)
       if [[ "$?" -eq "0" ]]; then
-        echo "  Spacelift check"
+        LABEL_NEEDED=1
+        echo "  Description check"
         echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].output.title | grep -q "${INPUTS_CHECK_DESCRIPTION}"
-        # if the desired phrase isn't found, set PLAN_CHANGES=1 and exit all the nested loops
-        no_changes_yn=$?
-        if [[ "$no_changes_yn" -ne "0" ]]; then
-          echo "    changes found - exiting!"
-          PLAN_CHANGES=1
+        # if the desired phrase isn't found, set LABEL_NEEDED=0 again and exit all the nested loops
+        phrase_found_yn=$?
+        if [[ "$phrase_found_yn" -ne "0" ]]; then
+          echo "    check found without appropriate description - exiting!"
+          LABEL_NEEDED=0
           break 2
         else
-          echo "    no changes"
+          echo "    description found"
         fi
       else
         echo "  not a ${INPUTS_CHECK_NAME} check, moving on"
@@ -49,6 +52,6 @@ for check_suite in "${CHECK_SUITES_ARRAY[@]}"; do
   fi
 done
 
-# Now that we've determined the desired value of PLAN_CHANGES, let's export it for use in labeling the pull request.
-echo "Plan changes: $PLAN_CHANGES"
-echo "::set-output name=changes::${PLAN_CHANGES}"
+# Now that we've determined the desired value of LABEL_NEEDED, let's export it for use in labeling the pull request.
+echo "Label needed: $LABEL_NEEDED"
+echo "::set-output name=label_needed::${LABEL_NEEDED}"
