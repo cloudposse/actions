e808741197a2e53a70a32da086bb5cbb67f6b47e
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-05 18:18:09 -0500

    making add and rmove label steps pure bash

---
+++ github/status-check-labeling/add_pr_label.sh
@@ -0,0 +1,12 @@
+# I'm expecting some greps to fail, so we want to continue running whenever we encounter errors.
+set +e
+
+# We already know that the pull request should have the label of interest but doesn't. Let's rectify that.
+echo "Adding PR label."
+RESPONSE=$(curl \
+  -X POST \
+  -d '{"labels":["${LABEL}"]}'
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: access_token ${GITHUB_TOKEN}" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels)
+echo "response: $RESPONSE"
---
+++ github/status-check-labeling/remove_pr_label.sh
@@ -0,0 +1,11 @@
+# I'm expecting some greps to fail, so we want to continue running whenever we encounter errors.
+set +e
+
+# We already know that the pull request has our label of interest when it shouldn't. We'll rectify that.
+echo "Removing PR label."
+RESPONSE=$(curl \
+  -X DELETE \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: access_token ${GITHUB_TOKEN}" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels/${LABEL})
+echo "response: $RESPONSE"
