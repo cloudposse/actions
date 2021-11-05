8cbe9da7c0b6ed2f594b55042ab702b258a1a452
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-05 18:31:48 -0500

    debugging

--- github/status-check-labeling/determine_current_pr_labels.sh
+++ github/status-check-labeling/determine_current_pr_labels.sh
@@ -4,22 +4,16 @@ set +e
 # We're starting with a pull request and we want to determine whether it already has the label of interest.
 PR_LABELED=0
 PR_NUMBER=${GITHUB_EVENT_NUMBER}
-echo "pr_number: $PR_NUMBER"
-echo "github_repository: $GITHUB_REPOSITORY"
 # Get the number of labels.
 PR_INFO=$(curl -H "Authorization: access_token ${GITHUB_TOKEN}" https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER})
-echo "pr_info: $PR_INFO"
 NUMBER_OF_LABELS=$( echo "$PR_INFO" | jq '.labels | length' )
-echo "number_of_labels: $NUMBER_OF_LABELS"
 # Check each label in the PR to see if it's the label of interest.
 if [ "$NUMBER_OF_LABELS" -gt "0" ]; then
   # show all labels, for debugging purposes
   echo $PR_INFO | jq '.labels'
   # grep for ${INPUTS_LABEL} in the labels list
   LAST_LABEL_INDEX=$(($NUMBER_OF_LABELS-1))
-  echo $LAST_LABEL_INDEX
   for label_index in $(seq 0 $LAST_LABEL_INDEX); do
-    echo $label_index
     echo $PR_INFO | jq .labels[${label_index}].name
     echo $PR_INFO | jq .labels[${label_index}].name | grep -q "${INPUTS_LABEL}"
     label_yn=$?
