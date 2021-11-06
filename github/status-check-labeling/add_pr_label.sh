e39d8f94b11c3751d43ee97d5248b4dbfb4a6ff7
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-06 16:31:10 -0500

    adding label creation functionality

--- github/status-check-labeling/add_pr_label.sh
+++ github/status-check-labeling/add_pr_label.sh
@@ -1,6 +1,15 @@
 # I'm expecting some greps to fail, so we want to continue running whenever we encounter errors.
 set +e
 
+# Create the label of interest first, in case it doesn't already exist.
+CREATION_RESPONSE=$(curl \
+  -X POST \
+  -d '{"name":"${LABEL}"}'
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  https://api.github.com/repos/${GITHUB_REPOSITORY}/labels)
+echo "creation_response: $CREATION_RESPONSE"
+
 # We already know that the pull request should have the label of interest but doesn't. Let's rectify that.
 echo "Adding PR label."
 PR_NUMBER=${GITHUB_EVENT_NUMBER}
