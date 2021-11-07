c573f9944d85c067e265e5e6f19aec60d485ee51
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-06 21:01:31 -0500

    adding full wait loop for check suites

--- github/status-check-labeling/identify_required_pr_labels.sh
+++ github/status-check-labeling/identify_required_pr_labels.sh
@@ -24,55 +24,54 @@ CHECK_SUITES_NAMES=$(echo $CHECK_SUITES_INFO | jq .check_suites[].id)
 CHECK_SUITES_ARRAY=( $CHECK_SUITES_NAMES )
 # For each check suite, get the number of runs in that check suite
 for check_suite in "${CHECK_SUITES_ARRAY[@]}"; do
-  #echo "check_suite: $check_suite"
-  CHECK_SUITE_INFO=$(curl \
-    -H "Accept: application/vnd.github.v3+json" \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    https://api.github.com/repos/${GITHUB_REPOSITORY}/check-suites/${check_suite}/check-runs)
-  #echo "CHECK_SUITE_INFO:"
-  #cat ${CHECK_SUITE_INFO} | jq .
-  NUMBER_OF_RUNS=$( echo $CHECK_SUITE_INFO | jq '.check_runs | length')
-  # Check each run in the check suite for ${{ inputs.check-name }}ivity
-  if [ "$NUMBER_OF_RUNS" -gt "0" ]; then
-    # grep for ${{ inputs.check-name }} in the run names
-    LAST_RUN_INDEX=$(($NUMBER_OF_RUNS-1))
-    for run_index in $(seq 0 $LAST_RUN_INDEX); do
-      #echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}]
-      echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].name | grep -q "${INPUTS_CHECK_NAME}"
-      # If "${{ inputs.check-name }}" is found in the run name:
-      # First, sit in a loop and wait for the check to finish.
-      # After that, begin evaluating LABEL_NEEDED:
-      #     Initially, set LABEL_NEEDED=1 (since we only potentially need a label when the relevant checks actuall took place),
-      #     and then check this run's title for "${{ inputs.check-description }}" (when we might set LABEL_NEEDED back to 0)
-      if [[ "$?" -eq "0" ]]; then
-        # Unfortunately, we need to wait until the check is finished.
-        echo "Waiting for check to complete."
-        echo "current_status: $CURRENT_STATUS"
-        while [[ $CURRENT_STATUS != "completed" ]]; do
-          echo "waiting"
-          sleep 10
-          CURRENT_STATUS=$(echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].status)
+  while :
+  do
+    CHECK_SUITE_INFO=$(curl \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token ${GITHUB_TOKEN}" \
+      https://api.github.com/repos/${GITHUB_REPOSITORY}/check-suites/${check_suite}/check-runs)
+    NUMBER_OF_RUNS=$( echo $CHECK_SUITE_INFO | jq '.check_runs | length')
+    # Check each run in the check suite for ${{ inputs.check-name }}ivity
+    if [ "$NUMBER_OF_RUNS" -gt "0" ]; then
+      # grep for ${{ inputs.check-name }} in the run names
+      LAST_RUN_INDEX=$(($NUMBER_OF_RUNS-1))
+      for run_index in $(seq 0 $LAST_RUN_INDEX); do
+        echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].name | grep -q "${INPUTS_CHECK_NAME}"
+        # If "${{ inputs.check-name }}" is found in the run name:
+        # First, see if the check has finished. If not, wait 10 seconds and restart the evaluation of this entire check suite.
+        # After that, begin evaluating LABEL_NEEDED.
+        #   Initially, set LABEL_NEEDED=1 (since we only potentially need a label when the relevant checks actually took place),
+        #   and then check this run's title for "${{ inputs.check-description }}" (when we might set LABEL_NEEDED back to 0)
+        if [[ "$?" -eq "0" ]]; then
+          # Unfortunately, we need to wait until the check is finished.
+          echo "Waiting for check to complete."
           echo "current_status: $CURRENT_STATUS"
-        done
-        echo "Check completed."
-        # Now that we're sure the check is complete, let's compute LABEL_NEEDED.
-        LABEL_NEEDED=1
-        echo "  Description check"
-        echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].output.title | grep -q "${INPUTS_CHECK_DESCRIPTION}"
-        # if the desired phrase isn't found, set LABEL_NEEDED=0 again and exit all the nested loops
-        phrase_found_yn=$?
-        if [[ "$phrase_found_yn" -ne "0" ]]; then
-          echo "    check found without appropriate description - exiting!"
-          LABEL_NEEDED=0
-          break 2
+          if [[ $CURRENT_STATUS != "completed" ]]; then
+            echo "waiting"
+            sleep 10
+            continue
+          fi
+          echo "Check completed."
+          # Now that we're sure the check is complete, let's compute LABEL_NEEDED.
+          LABEL_NEEDED=1
+          echo "  Description check"
+          echo $CHECK_SUITE_INFO | jq .check_runs[${run_index}].output.title | grep -q "${INPUTS_CHECK_DESCRIPTION}"
+          # if the desired phrase isn't found, set LABEL_NEEDED=0 again and exit all the nested loops
+          phrase_found_yn=$?
+          if [[ "$phrase_found_yn" -ne "0" ]]; then
+            echo "    check found without appropriate description - exiting!"
+            LABEL_NEEDED=0
+            break 3
+          else
+            echo "    description found"
+          fi
         else
-          echo "    description found"
+          echo "  not a ${INPUTS_CHECK_NAME} check, moving on"
         fi
-      else
-        echo "  not a ${INPUTS_CHECK_NAME} check, moving on"
-      fi
-    done
-  fi
+      done
+    fi
+    break
+  done
 done
 
 # Now that we've determined the desired value of LABEL_NEEDED, let's export it for use in labeling the pull request.
