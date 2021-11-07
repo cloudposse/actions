0b6c9e24ceb28e6793be380409c60fbc9035f56b
Dylan Bannon <dylanbannon5@gmail.com>
2021-11-06 22:05:18 -0500

    debugging label addition script

--- github/status-check-labeling/add_pr_label.sh
+++ github/status-check-labeling/add_pr_label.sh
@@ -2,9 +2,10 @@
 set +e
 
 # Create the label of interest first, in case it doesn't already exist.
+creation_post_data="{\"name\":\"${LABEL}\"}"
 CREATION_RESPONSE=$(curl -s \
   -X POST \
-  -d '{"name":"${LABEL}"}' \
+  -d "$creation_post_data" \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/labels)
@@ -13,9 +14,10 @@ echo "creation_response: $CREATION_RESPONSE"
 # We already know that the pull request should have the label of interest but doesn't. Let's rectify that.
 echo "Adding PR label."
 PR_NUMBER=${GITHUB_EVENT_NUMBER}
+addition_post_data="{\"labels\":[\"${LABEL}\"]}"
 RESPONSE=$(curl -s \
   -X POST \
-  -d '{"labels":["${LABEL}"]}' \
+  -d "$addition_post_data" \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/labels)
