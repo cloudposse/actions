8fd194c7e6decc2ff85ca64de59b288983a2af91
Nuru <Nuru@users.noreply.github.com>
2023-03-13 12:54:31 -0700

    Update cloned actions to current

---
+++ github/auto-approve/src/approve.test.ts
@@ -0,0 +1,343 @@
+import * as core from "@actions/core";
+import { Context } from "@actions/github/lib/context";
+import { create } from "domain";
+import nock from "nock";
+import { approve } from "./approve";
+
+const originalEnv = process.env;
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+  jest.spyOn(core, "setFailed").mockImplementation(jest.fn());
+  jest.spyOn(core, "info").mockImplementation(jest.fn());
+  nock.disableNetConnect();
+
+  process.env = { GITHUB_REPOSITORY: "hmarr/test" };
+});
+
+afterEach(() => {
+  nock.cleanAll();
+  nock.enableNetConnect();
+  process.env = originalEnv;
+});
+
+const apiNock = nock("https://api.github.com");
+const apiMocks = {
+  getUser: (status?: number, body?: object) =>
+    apiNock.get("/user").reply(status ?? 200, body ?? { login: "hmarr" }),
+  getPull: (status?: number, body?: object) =>
+    apiNock
+      .get("/repos/hmarr/test/pulls/101")
+      .reply(
+        status ?? 200,
+        body ?? { head: { sha: "24c5451bbf1fb09caa3ac8024df4788aff4d4974" } }
+      ),
+  getReviews: (status?: number, body?: any) =>
+    apiNock
+      .get("/repos/hmarr/test/pulls/101/reviews")
+      .reply(status ?? 200, body ?? []),
+  createReview: () =>
+    apiNock.post("/repos/hmarr/test/pulls/101/reviews").reply(200, {}),
+};
+
+test("a review is successfully created with a PAT", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext());
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("a review is successfully created with an Actions token", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext());
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review is successfully created with message", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext(), undefined, "Review body");
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review is successfully created using pull-request-number", async () => {
+  apiMocks.getUser();
+  apiNock
+    .get("/repos/hmarr/test/pulls/102")
+    .reply(200, { head: { sha: "24c5451bbf1fb09caa3ac8024df4788aff4d4974" } });
+  apiNock.get("/repos/hmarr/test/pulls/102/reviews").reply(200, []);
+
+  const createReview = apiNock
+    .post("/repos/hmarr/test/pulls/102/reviews")
+    .reply(200, { id: 1 });
+
+  await approve("gh-tok", new Context(), 102);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review has already been approved by current user", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "hmarr" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "APPROVED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext());
+
+  expect(createReview.isDone()).toBe(false);
+  expect(core.info).toHaveBeenCalledWith(
+    expect.stringContaining(
+      "Current user already approved pull request #101, nothing to do"
+    )
+  );
+});
+
+test("when a review is pending", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "hmarr" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "PENDING",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review is dismissed", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "hmarr" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "DISMISSED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review is not approved", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "hmarr" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "CHANGES_REQUESTED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review is commented", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "hmarr" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "COMMENTED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review has already been approved by another user", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "some" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "APPROVED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review has already been approved by unknown user", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews(200, [
+    {
+      user: null,
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "APPROVED",
+    },
+  ]);
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("when a review has been previously approved by user and but requests a re-review", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull(200, {
+    head: { sha: "24c5451bbf1fb09caa3ac8024df4788aff4d4974" },
+    requested_reviewers: [{ login: "hmarr" }],
+  });
+  apiMocks.getReviews(200, [
+    {
+      user: { login: "some" },
+      commit_id: "24c5451bbf1fb09caa3ac8024df4788aff4d4974",
+      state: "APPROVED",
+    },
+  ]);
+
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", new Context(), 101);
+
+  expect(createReview.isDone()).toBe(true);
+});
+
+test("without a pull request", async () => {
+  const createReview = apiMocks.createReview();
+  await approve("gh-tok", new Context());
+
+  expect(createReview.isDone()).toBe(false);
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("Make sure you're triggering this")
+  );
+});
+
+test("when the token is invalid", async () => {
+  apiMocks.getUser(401, { message: "Bad credentials" });
+  apiMocks.getPull(401, { message: "Bad credentials" });
+  apiMocks.getReviews(401, { message: "Bad credentials" });
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext());
+
+  expect(createReview.isDone()).toBe(false);
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("`github-token` input parameter")
+  );
+});
+
+test("when the token doesn't have write permissions", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  apiNock
+    .post("/repos/hmarr/test/pulls/101/reviews")
+    .reply(403, { message: "Resource not accessible by integration" });
+
+  await approve("gh-tok", ghContext());
+
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("pull_request_target")
+  );
+});
+
+test("when a user tries to approve their own pull request", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  apiNock
+    .post("/repos/hmarr/test/pulls/101/reviews")
+    .reply(422, { message: "Unprocessable Entity" });
+
+  await approve("gh-tok", ghContext());
+
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("same user account")
+  );
+});
+
+test("when pull request does not exist or the token doesn't have access", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull(404, { message: "Not Found" });
+  apiMocks.getReviews(404, { message: "Not Found" });
+  const createReview = apiMocks.createReview();
+
+  await approve("gh-tok", ghContext());
+
+  expect(createReview.isDone()).toBe(false);
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("doesn't have access")
+  );
+});
+
+test("when the token is read-only", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  apiNock
+    .post("/repos/hmarr/test/pulls/101/reviews")
+    .reply(403, { message: "Not Authorized" });
+
+  await approve("gh-tok", ghContext());
+
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("are read-only")
+  );
+});
+
+test("when the token doesn't have write access to the repository", async () => {
+  apiMocks.getUser();
+  apiMocks.getPull();
+  apiMocks.getReviews();
+  apiNock
+    .post("/repos/hmarr/test/pulls/101/reviews")
+    .reply(404, { message: "Not Found" });
+
+  await approve("gh-tok", ghContext());
+
+  expect(core.setFailed).toHaveBeenCalledWith(
+    expect.stringContaining("doesn't have access")
+  );
+});
+
+function ghContext(): Context {
+  const ctx = new Context();
+  ctx.payload = {
+    pull_request: {
+      number: 101,
+    },
+  };
+  return ctx;
+}
--- github/repository-dispatch/README.md
+++ github/repository-dispatch/README.md
@@ -6,15 +6,8 @@ A GitHub action to create a repository dispatch event.
 
 ## Usage
 
-Dispatch an event to the current repository by elivating the permissions of the default `GITHUB_TOKEN`.
+Dispatch an event to the current repository.
 ```yml
-permissions:
-  actions: write
-
-jobs:
-  repositorydispatch:
-    runs-on: ubuntu-latest
-    steps:
       - name: Repository Dispatch
         uses: peter-evans/repository-dispatch@v2
         with:
@@ -26,7 +19,7 @@ Dispatch an event to a remote repository using a `repo` scoped [Personal Access 
       - name: Repository Dispatch
         uses: peter-evans/repository-dispatch@v2
         with:
-          token: ${{ secrets.REPO_ACCESS_TOKEN }}
+          token: ${{ secrets.PAT }}
           event-type: my-event
 ```
 
@@ -34,7 +27,7 @@ Dispatch an event to a remote repository using a `repo` scoped [Personal Access 
 
 | Name | Description | Default |
 | --- | --- | --- |
-| `token` | (**required**) `GITHUB_TOKEN` (permissions `actions: write`) or a `repo` scoped [Personal Access Token (PAT)](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token). See [token](#token) for further details. | `GITHUB_TOKEN` |
+| `token` | `GITHUB_TOKEN` (permissions `contents: write`) or a `repo` scoped [Personal Access Token (PAT)](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token). See [token](#token) for further details. | `GITHUB_TOKEN` |
 | `repository` | The full name of the repository to send the dispatch. | `github.repository` (current repository) |
 | `event-type` | (**required**) A custom webhook event name. | |
 | `client-payload` | JSON payload with extra information about the webhook event that your action or workflow may use. | `{}` |
@@ -43,12 +36,6 @@ Dispatch an event to a remote repository using a `repo` scoped [Personal Access 
 
 This action creates [`repository_dispatch`](https://docs.github.com/en/rest/repos/repos#create-a-repository-dispatch-event) events.
 The default `GITHUB_TOKEN` token can only be used if you are dispatching the same repository that the workflow is executing in.
-In this case you must [elevate the token permissions](https://docs.github.com/en/actions/using-jobs/assigning-permissions-to-jobs) to allow the dispatch.
-
-```yaml
-permissions:
-  actions: write
-```
 
 To dispatch to a remote repository you must create a [Personal Access Token (PAT)](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) with the `repo` scope and store it as a secret.
 If you will be dispatching to a public repository then you can use the more limited `public_repo` scope.
@@ -61,7 +48,7 @@ Here is an example setting all of the input parameters.
       - name: Repository Dispatch
         uses: peter-evans/repository-dispatch@v2
         with:
-          token: ${{ secrets.REPO_ACCESS_TOKEN }}
+          token: ${{ secrets.PAT }}
           repository: username/my-repo
           event-type: my-event
           client-payload: '{"ref": "${{ github.ref }}", "sha": "${{ github.sha }}"}'
@@ -104,7 +91,7 @@ jobs:
       - name: Repository Dispatch
         uses: peter-evans/repository-dispatch@v2
         with:
-          token: ${{ secrets.REPO_ACCESS_TOKEN }}
+          token: ${{ secrets.PAT }}
           repository: ${{ matrix.repo }}
           event-type: my-event
 ```
